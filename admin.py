bb3f4dbab4c6eda4f255bfd75bf739a7351cc330
Jan Hendrik Dreyer <68108804+JHDreyer@users.noreply.github.com>
2020-08-01 00:10:34 +0200

    Revert "Advertising"

--- admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from . models import Platforms
-# Register your models here.
-
-class PlatfromAdmin(admin.ModelAdmin):
-     list_display = ('facebook', 'instagram','google', 'budget', 'budgetperiod')
-
-admin.site.register(Platforms, PlatfromAdmin)
