b472054054290e8103ba9581efb74c2bc0783390
JHDreyer <jhdreyer8@gmail.com>
2020-08-01 20:22:14 +0200

    Minor Fixes

--- businesses/urls.py
+++ businesses/urls.py
@@ -19,19 +19,19 @@ urlpatterns = [
 
     path('<int:pk>/product/',
          views.add_product_to_business, name='pcreate'),
-    path('product/list/', views.BusinessListView.as_view(), name='plist'),
+    path('product/list/', views.ProductListView.as_view(), name='plist'),
     path('product/<int:pk>/update/',
-         views.BusinessUpdateView.as_view(), name='pupdate'),
+         views.ProductUpdateView.as_view(), name='pupdate'),
     path('product/<int:pk>/delete/',
-         views.BusinessDeleteView.as_view(), name='pdelete'),
+         views.ProductDeleteView.as_view(), name='pdelete'),
 
     path('<int:pk>/service/',
          views.add_service_to_business, name='screate'),
-    path('service/list/', views.BusinessListView.as_view(), name='slist'),
+    path('service/list/', views.ServiceListView.as_view(), name='slist'),
     path('service/<int:pk>/update/',
-         views.BusinessUpdateView.as_view(), name='supdate'),
+         views.ServiceUpdateView.as_view(), name='supdate'),
     path('service/<int:pk>/delete/',
-         views.BusinessDeleteView.as_view(), name='sdelete'),
+         views.ServiceDeleteView.as_view(), name='sdelete'),
 
 
 ]
